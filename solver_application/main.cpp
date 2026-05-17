d309dcbf3a121bca37ad60d10707e9f72d667328
vxntvs <pavelakhmetov147@gmail.com>
2026-05-17 22:46:37 +0300

    homework: implemented formatter_lib and solver application

---
+++ solver_application/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "../formatter_lib/formatter.h"
+
+int main() {
+    // Вызываем твою функцию formatter и выводим результат в консоль
+    std::cout << formatter("Hello from Solver Application with CPack!") << std::endl;
+    return 0;
+}
