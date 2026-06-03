fb30cb21d14bd8e8ad3385034a12f79c539c5d71
vxntvs <pavelakhmetov147@gmail.com>
2026-06-03 21:22:48 +0300

    Add demo app, Polly submodule, and GitHub Actions workflow

---
+++ demo/main.cpp
@@ -0,0 +1,21 @@
+#include <print.hpp>
+#include <cstdlib>
+#include <fstream>
+#include <iostream>
+
+int main(int argc, char* argv[])
+{
+  const char* log_path = std::getenv("LOG_PATH");
+  if (log_path == nullptr)
+  {
+    std::cerr << "undefined environment variable: LOG_PATH" << std::endl;
+    return 1;
+  }
+  std::string text;
+  while (std::cin >> text)
+  {
+    std::ofstream out{log_path, std::ios_base::app};
+    print(text, out);
+    out << std::endl;
+  }
+}
